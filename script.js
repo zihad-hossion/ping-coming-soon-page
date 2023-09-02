81aef638b8b6cd06f10e82df7bbde099cb3df627
Zihad Hossion <126801486+zihad-hossion@users.noreply.github.com>
2023-09-02 20:50:41 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const eMail = document.querySelector("#email");
+const submitBtn = document.querySelector("button");
+const errTxt = document.querySelector(".error-text");
+
+submitBtn.addEventListener("click", () => {
+    if (isValidEmail(eMail.value)) {
+        errTxt.style.display = "none";
+    } else {
+        errTxt.style.display = "block";
+        eMail.style.border = "1px solid rgb(180, 86, 86)";
+    }
+
+})
+
+function isValidEmail(email) {
+    // Regular expression pattern for basic email validation
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+}
